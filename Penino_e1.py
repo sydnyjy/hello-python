ab69672f2d618ffb1e128930e63c640670be7732
Sydney Penino <sydnyjy@gmail.com>
2018-10-25 22:52:11 +0800

    Penino_e1.py

---
+++ Penino_e1.py
@@ -0,0 +1,12 @@
+data ={ "Name" : "Sydney Joy J. Penino",
+"Spirit Animal" : "Tiger",
+"Reason" : "I am tough ",
+"Hobby" : "Read books",
+"Profession" : "System Analyst"
+
+}
+print ("I am {}".format(data["Name"]))
+print ("My spirit animal is {}".format(data["Spirit Animal"]))
+print ("Because {}".format(data["Reason"]))
+print ("When not in School, I love to {}".format(data["Hobby"]))
+print ("I dream of being a {}".format(data["Profession"]))
--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
 print ("Hello, World")
 print ("My name is {}. I am {} y/o." \
 .format("Groot", 16))
+
+print ("AdNU", 2018, sep="-",  end="")
+print("CSNHS")
