6d39256592b7a66959589eb029936a298da1271f
Sydney Penino <sydnyjy@gmail.com>
2018-10-26 17:08:21 +0800

    E3

--- Penino_e3.py
+++ Penino_e3.py
@@ -1,8 +1,12 @@
-Input_number =input ("Ener a comma separated list of numbers:") 
+Input_number = input ("Ener a comma separated list of numbers:") 
+print(type((Input_number)))
+user_input= Input_number.split(",")
+print(type((user_input)))
 
-
- x = 0 
-while x<4:
-    # if x**2:
-    print ("Sum of squares: {}" + )
-    x+= 1
+index=0
+sum=0  
+for x in user_input:
+    sum += float (user_input[index])**2
+    index+=1
+    print ("Sum of squares:" , sum )
+    
